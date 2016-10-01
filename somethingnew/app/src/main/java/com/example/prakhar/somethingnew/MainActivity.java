507e9c1232c4d103bf785c087fc30cceb0cd888a
pkhrag <pkhrag@iitk.ac.in>
2016-10-02 01:03:12 +0530

    added navigation between pages

--- somethingnew/app/src/main/java/com/example/prakhar/somethingnew/MainActivity.java
+++ somethingnew/app/src/main/java/com/example/prakhar/somethingnew/MainActivity.java
@@ -4,12 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final  TAG = "manuMessage";
+    private static final String TAG = "manuMessage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- somethingnew/app/src/main/java/com/example/prakhar/somethingnew/new_semester.java
+++ somethingnew/app/src/main/java/com/example/prakhar/somethingnew/new_semester.java
@@ -2,6 +2,8 @@ package com.example.prakhar.somethingnew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class new_semester extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class new_semester extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_semester);
+    }
+
+    public void departmental(View view) {
+        Intent depart = new Intent(this, Department_courses.class);
+        startActivity(depart);
+    }
+
+    public void so(View view) {
+        Intent SO = new Intent(this, Main2Activity_so.class);
+        startActivity(SO);
+    }
+
+    public void eso(View view) {
+        Intent ESO = new Intent(this, eso_page.class);
+        startActivity(ESO);
     }
 }
