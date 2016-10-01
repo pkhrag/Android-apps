ea1f17c543e6ed70993950a5e58c7785d79d22ee
dsinghvi <divyanshsinghvi@gmail.com>
2016-10-01 21:57:44 +0530

    main page

--- somethingnew/app/src/main/java/com/example/prakhar/somethingnew/MainActivity.java
+++ somethingnew/app/src/main/java/com/example/prakhar/somethingnew/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.Main_Page);
         Log.i(TAG,"onCreate");
         Button LogInButton = (Button)findViewById(R.id.button);
 
