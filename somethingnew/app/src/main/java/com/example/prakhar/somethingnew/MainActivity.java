39e403b2deef4ef6f81dd8272ae5a1a2077fcb5d
pkhrag <pkhrag@iitk.ac.in>
2016-10-01 23:24:11 +0530

    fixed a few bugs

--- somethingnew/app/src/main/java/com/example/prakhar/somethingnew/MainActivity.java
+++ somethingnew/app/src/main/java/com/example/prakhar/somethingnew/MainActivity.java
@@ -14,34 +14,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Main_Page);
+        setContentView(R.layout.main_page);
         Log.i(TAG,"onCreate");
-        Button LogInButton = (Button)findViewById(R.id.button);
 
-        LogInButton.setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onClick(View v){
-                        TextView text = (TextView)findViewById(R.id.textView1);
-                        text.setText("Done Job!");
-                    }
-                }
-        );
-
-        LogInButton.setOnLongClickListener(
-                new Button.OnLongClickListener(){
-                    public boolean onLongClick(View v){
-                        TextView text = (TextView)findViewById(R.id.textView1);
-                        text.setText("Don't press so long");
-                        return true; // if you return false, it will only show when the button is pressed not when released.
-                    }
-                }
-        );
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        TextView text = (TextView)findViewById(R.id.textView1);
-        text.setText("Press the button");
-    }
 }
