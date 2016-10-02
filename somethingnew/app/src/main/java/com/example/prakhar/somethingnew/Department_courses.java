0edd4cad5e13717e2e1f778b9ac06e63e5bd3aea
pkhrag <pkhrag@iitk.ac.in>
2016-10-02 09:44:34 +0530

    page navigation

--- somethingnew/app/src/main/java/com/example/prakhar/somethingnew/Department_courses.java
+++ somethingnew/app/src/main/java/com/example/prakhar/somethingnew/Department_courses.java
@@ -1,7 +1,9 @@
 package com.example.prakhar.somethingnew;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Department_courses extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class Department_courses extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_department_courses);
+    }
+    public void aero (View view) {
+        Intent depart = new Intent(this, Main2Activity_aerospace.class);
+        startActivity(depart);
+    }
+
+    public void cse (View view) {
+        Intent depart = new Intent(this, Main2Activity_cs.class);
+        startActivity(depart);
+    }
+
+    public void mth (View view) {
+        Intent depart = new Intent(this, Main2Activity_mth.class);
+        startActivity(depart);
+    }
+
+    public void ee (View view) {
+        Intent depart = new Intent(this, Main2Activity_EE.class);
+        startActivity(depart);
     }
 }
--- somethingnew/app/src/main/java/com/example/prakhar/somethingnew/Main2Activity_cse.java
+++ somethingnew/app/src/main/java/com/example/prakhar/somethingnew/Main2Activity_cse.java
@@ -2,7 +2,7 @@ package com.example.prakhar.somethingnew;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
+
 import android.widget.TextView;
 
 public class Main2Activity_cse extends Activity {
@@ -12,13 +12,7 @@ public class Main2Activity_cse extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2_cse);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
+        setContentView(R.layout.activity_main2_cs);
+
     }
 }
