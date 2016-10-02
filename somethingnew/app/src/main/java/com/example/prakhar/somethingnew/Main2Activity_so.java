b38bbd3afc31b25120aafd0638b95b62948f3b83
pkhrag <pkhrag@iitk.ac.in>
2016-10-02 08:26:59 +0530

    added backend for choices submission

--- somethingnew/app/src/main/java/com/example/prakhar/somethingnew/Main2Activity_so.java
+++ somethingnew/app/src/main/java/com/example/prakhar/somethingnew/Main2Activity_so.java
@@ -2,6 +2,9 @@ package com.example.prakhar.somethingnew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.content.Intent;
 
 public class Main2Activity_so extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class Main2Activity_so extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2_so);
+    }
+
+    public void onClick(View view) {
+        Intent new_sem = new Intent(this, new_semester.class);
+        CheckBox mso201 = (CheckBox) findViewById(R.id.MSO201);
+        CheckBox mso202 = (CheckBox) findViewById(R.id.MSO202);
+        CheckBox mso203 = (CheckBox) findViewById(R.id.MSO203);
+        CheckBox cso201 = (CheckBox) findViewById(R.id.CSO201);
+        CheckBox cso202 = (CheckBox) findViewById(R.id.CSO202);
+        CheckBox pso201 = (CheckBox) findViewById(R.id.PSO201);
+
+        new_sem.putExtra("MSO201",mso201.isChecked());
+        new_sem.putExtra("MSO202",mso202.isChecked());
+        new_sem.putExtra("MSO203",mso203.isChecked());
+        new_sem.putExtra("CSO201",cso201.isChecked());
+        new_sem.putExtra("CSO202",cso202.isChecked());
+        new_sem.putExtra("PSO201",pso201.isChecked());
+
+        startActivity(new_sem);
     }
 }
--- somethingnew/app/src/main/java/com/example/prakhar/somethingnew/eso_page.java
+++ somethingnew/app/src/main/java/com/example/prakhar/somethingnew/eso_page.java
@@ -1,7 +1,10 @@
 package com.example.prakhar.somethingnew;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class eso_page extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class eso_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eso_page);
+    }
+
+    public void onClick(View view) {
+        Intent new_sem = new Intent(this, new_semester.class);
+        CheckBox eso201a = (CheckBox) findViewById(R.id.ESO201A);
+        CheckBox eso202a = (CheckBox) findViewById(R.id.ESO202A);
+        CheckBox eso203a = (CheckBox) findViewById(R.id.ESO203A);
+        CheckBox eso204a = (CheckBox) findViewById(R.id.ESO204A);
+        CheckBox eso205a = (CheckBox) findViewById(R.id.ESO205A);
+        CheckBox eso206a = (CheckBox) findViewById(R.id.ESO206A);
+        CheckBox eso207a = (CheckBox) findViewById(R.id.ESO207A);
+        CheckBox eso208a = (CheckBox) findViewById(R.id.ESO208A);
+        CheckBox eso209a = (CheckBox) findViewById(R.id.ESO209A);
+
+        new_sem.putExtra("ESO201A",eso201a.isChecked());
+        new_sem.putExtra("ESO202A",eso202a.isChecked());
+        new_sem.putExtra("ESO203A",eso203a.isChecked());
+        new_sem.putExtra("ESO204A",eso204a.isChecked());
+        new_sem.putExtra("ESO205A",eso205a.isChecked());
+        new_sem.putExtra("ESO206A",eso206a.isChecked());
+        new_sem.putExtra("ESO207A",eso207a.isChecked());
+        new_sem.putExtra("ESO208A",eso208a.isChecked());
+        new_sem.putExtra("ESO209A",eso209a.isChecked());
+
+        startActivity(new_sem);
     }
 }
